053d2e7de90c57d9a1cdbd4f7eecf6fcb84dbe48
dahl <dahl@a8e578cc-b067-0410-9529-a28e5a6d5a41>
2012-12-13 02:17:31 +0000

    - Grab object size and modified date from Solr instead of CNRead.describe(). Accessing science objects works now. - Capture errors that come from HTTPSConnection, such as timeouts. - Capture unknown exceptions that come from httplib.

--- src/impl/onedrive_d1_client.py
+++ src/impl/onedrive_d1_client.py
@@ -29,6 +29,7 @@
 
 # Stdlib.
 import logging
+import ssl
 
 # D1.
 import d1_common
@@ -85,6 +86,8 @@ class D1Client(object):
       return self.client.describe(pid)
     except d1_common.types.exceptions.DataONEException as e:
       raise path_exception.PathException(e.description)
+    except (ssl.SSLError, Exception) as e:
+      raise path_exception.PathException(str(e))
 
 
   def get_science_object(self, pid):
@@ -93,13 +96,16 @@ class D1Client(object):
       return d1client.get(pid)
     except d1_common.types.exceptions.DataONEException as e:
       raise path_exception.PathException(e.description)
-
+    except (ssl.SSLError, Exception) as e:
+      raise path_exception.PathException(str(e))
 
   def get_system_metadata(self, pid):
     try:
       return self.client.getSystemMetadata(pid)
     except d1_common.types.exceptions.DataONEException as e:
       raise path_exception.PathException(e.description)
+    except (ssl.SSLError, Exception) as e:
+      raise path_exception.PathException(str(e))
 
 
 # USED
